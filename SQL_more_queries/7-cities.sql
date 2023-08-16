e780338bbf5dddfdf439983d78759f1145c79295
Samson1340 <samsonsimeon2004@gmail.com>
2023-08-16 09:43:31 +0300

    update

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,10 +1,10 @@
 -- This is for database creation with table 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities
+CREATE TABLE IF NOT EXISTS `cities`
 (
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
 name VARCHAR(256),
 state_id INT NOT NULL, 
 FOREIGN KEY (state_id) REFERENCES state(id)
-)
+);
